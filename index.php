929f5abd1fad3628b54459d92e9b057433d5fd0b
Orkan <orkans@gmail.com>
2013-03-14 14:23:03 +0100

    3.6 Git Branching - Rebasing

--- index.php
+++ index.php
@@ -8,21 +8,10 @@ if(!empty($param)) {
 }
 else {
 	/**
-	 * Some comment
-	 * @Function: dsds
-	 * iss91: C2
-	 * iss91: C4
-	 * iss91: C7
-	 * iss91: C8
-	 * iss91: C11
+	 * master branch
 	 */
 	echo "Cannot render - changed! HotFIX here!!!";
 	/**
-	 * master: C3
-	 * master: C9
-	 * master: C10
-	 * dumbidea: C12
-	 * dumbidea: C13
-	 * master: C14
+	 * experiment branch
 	 */
 }
